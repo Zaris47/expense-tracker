b5ff6e2143f0601bc7e67c17389a52ec96f2c43a
Zaris47 <akhildasmisterio@gmail.com>
2022-11-23 19:34:42 +0530

    Adding form and stuff

---
+++ src/components/NewExpense/NewExpense.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './NewExpense.css'
+
+const NewExpense = () => {
+  return (
+    <div className="new-expense">
+      <form action=""></form>
+    </div>
+  );
+};
+
+export default NewExpense;
